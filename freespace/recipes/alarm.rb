982f4148d6fa2388c09c1a3e2370c842bbecc866
Mark Tomzynski <tomzynsk@amazon.com>
2014-08-27 06:50:44 +0000

    fixed remote_file source

--- freespace/recipes/alarm.rb
+++ freespace/recipes/alarm.rb
@@ -13,5 +13,5 @@ end
 
 Chef::Log.info("FreeSpaceMonitor: Creating Alarm")
 execute 'create alarm' do
-    command "aws cloudwatch put-metric-alarm --alarm-name '#{node[:stack][:name]}-#{node[:instance][:hostname]}-UtilizedSpace' --alarm-description 'Utilized Space exceeds 90% on root on #{node[:instance][:aws_instance_id]}' --metric-name DiskSpaceAvailable --namespace 'Linux System' --statistic Maximum --period 300 --threshold 90 --comparison-operator GreaterThanThreshold --dimensions Name=InstanceId,Value=#{node[:instance][:aws_instance_id]} --evaluation-periods 1 --alarm-actions #{node[:opsworks][:snsarn]} --unit Percent --region #{node[:instance][:aws_instance_id]}"
+    command "aws cloudwatch put-metric-alarm --alarm-name '#{node[:opsworks][:stack][:name]}-#{node[:opsworks][:instance][:hostname]}-UtilizedSpace' --alarm-description 'Utilized Space exceeds 90% on root on #{node[:opsworks][:instance][:aws_instance_id]}' --metric-name DiskSpaceAvailable --namespace 'Linux System' --statistic Maximum --period 300 --threshold 90 --comparison-operator GreaterThanThreshold --dimensions Name=InstanceId,Value=#{node[:opsworks][:instance][:aws_instance_id]} --evaluation-periods 1 --alarm-actions #{node[:opsworks][:snsarn]} --unit Percent --region #{node[:opsworks][:instance][:aws_instance_id]}"
 end
