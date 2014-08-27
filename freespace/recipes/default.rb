0ab2752283271fc91dd5e6049977ed1154a3e481
Mark Tomzynski <tomzynsk@amazon.com>
2014-08-27 08:50:24 +0000

    Check if Directory Exists before attempting to get Scripts

--- freespace/recipes/default.rb
+++ freespace/recipes/default.rb
@@ -1,32 +1,34 @@
-Chef::Log.info("FreeSpaceMonitor: Install Required Packages")
-if platform?('centos', 'redhat', 'fedora', 'amazon')
-  package "perl-Switch"
-  package "perl-Sys-Syslog"
-  package "perl-LWP-Protocol-https"
-end
+unless Dir.exist? "/etc/aws-scripts-mon"
+  Chef::Log.info("FreeSpaceMonitor: Install Required Packages")
+  if platform?('centos', 'redhat', 'fedora', 'amazon')
+    package "perl-Switch"
+    package "perl-Sys-Syslog"
+    package "perl-LWP-Protocol-https"
+  end
 
-if platform?("debian","ubuntu")
-  package "unzip"
-  package "libwww-perl"
-  package "libcrypt-ssleay-perl"
-  package "libswitch-perl"
-end
+  if platform?("debian","ubuntu")
+    package "unzip"
+    package "libwww-perl"
+    package "libcrypt-ssleay-perl"
+    package "libswitch-perl"
+  end
 
-Chef::Log.info("FreeSpaceMonitor: Getting Scripts")
-remote_file "/tmp/CloudWatchMonitoringScripts-v1.1.0.zip" do
-  source "http://ec2-downloads.s3.amazonaws.com/cloudwatch-samples/CloudWatchMonitoringScripts-v1.1.0.zip"
-end
+  Chef::Log.info("FreeSpaceMonitor: Getting Scripts")
+  remote_file "/tmp/CloudWatchMonitoringScripts-v1.1.0.zip" do
+    source "http://ec2-downloads.s3.amazonaws.com/cloudwatch-samples/CloudWatchMonitoringScripts-v1.1.0.zip"
+  end
 
-Chef::Log.info("FreeSpaceMonitor: Unzip Scripts")
-bash "unzipScript" do
-  code <<-EOS
-  unzip /tmp/CloudWatchMonitoringScripts-v1.1.0.zip -d /etc
-  EOS
-end
+  Chef::Log.info("FreeSpaceMonitor: Unzip Scripts")
+  bash "unzipScript" do
+    code <<-EOS
+      unzip /tmp/CloudWatchMonitoringScripts-v1.1.0.zip -d /etc
+    EOS
+  end
 
-Chef::Log.info("FreeSpaceMonitor: Clean up zip")
-file "/tmp/CloudWatchMonitoringScripts-v1.1.0.zip" do
-  action :delete
+  Chef::Log.info("FreeSpaceMonitor: Clean up zip")
+  file "/tmp/CloudWatchMonitoringScripts-v1.1.0.zip" do
+    action :delete
+  end
 end
 
 Chef::Log.info("FreeSpaceMonitor: Add Cron Job")
